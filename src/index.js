286f53b0254330efe410d7b7c28c69d1638ffa26
Aiandrox <aiandrox7@gmail.com>
2022-07-09 17:28:45 +0900

    トリガーを増やす

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ try {
   console.log(`Hello ${nameToGreet}!`);
   const time = new Date().toTimeString();
   core.setOutput("time", time);
+
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
