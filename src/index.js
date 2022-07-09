e6771ae5558dba4ec2e44712edfd9bbb381480d2
Aiandrox <aiandrox7@gmail.com>
2022-07-09 17:24:11 +0900

    それっぽく書いた

--- src/index.js
+++ src/index.js
@@ -15,6 +15,18 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
+
+  const github_token = core.getInput("GITHUB_TOKEN");
+  const octokit = github.getOctokit(github_token);
+  const pull_number = context.payload.pull_request.number;
+  const message = "LGTM";
+
+  const { comment } = await octokit.rest.issues.createComment({
+    ...context.repo,
+    issue_number: pull_number,
+    body: message,
+  });
+  console.log({ comment });
 } catch (error) {
   core.setFailed(error.message);
 }
