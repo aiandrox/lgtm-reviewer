c9801a734edac3db5b40d39a7e8e5cc26ff601e8
Aiandrox <aiandrox7@gmail.com>
2022-07-09 17:08:09 +0900

    test

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+import { context, GitHub } from "@actions/github";
 
 try {
+  if (context.eventName !== "pull_request") {
+    // eslint-disable-next-line no-console
+    console.warn(`event name is not 'pull_request': ${context.eventName}`);
+    return;
+  }
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
+  const nameToGreet = core.getInput("who-to-greet");
   console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
+  const time = new Date().toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const payload = JSON.stringify(context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
