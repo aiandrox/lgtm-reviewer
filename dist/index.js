7ecb6f7d841968df3c1bffab1695164d063f446c
Aiandrox <aiandrox7@gmail.com>
2022-07-10 09:28:19 +0900

    debug

--- dist/index.js
+++ dist/index.js
@@ -8880,7 +8880,7 @@ const run = async () => {
         // const payload = JSON.stringify(context.payload, undefined, 2);
         // console.log(`The event payload: ${payload}`);
         const github_token = core.getInput("GITHUB_TOKEN");
-        console.log(github_token);
+        console.log(`github_token: ${github_token}`);
         const octokit = (0, github_1.getOctokit)(github_token);
         const pull_number = github_1.context.payload.pull_request.number;
         console.log(pull_number);
