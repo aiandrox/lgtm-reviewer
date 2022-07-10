3d63d7070aa01f2a1e2a14ff9070a7869c5ff3b5
Aiandrox <aiandrox7@gmail.com>
2022-07-10 09:53:13 +0900

    動作確認はしたので一旦コメントアウト https://github.com/aiandrox/lgtm-reviewer/pull/19

--- dist/index.js
+++ dist/index.js
@@ -8871,25 +8871,29 @@ const run = async () => {
             console.warn(`event name is not 'pull_request': ${github_1.context.eventName}`);
             return;
         }
-        const github_token = core.getInput("GITHUB_TOKEN");
-        const octokit = (0, github_1.getOctokit)(github_token);
-        const pull_number = github_1.context.payload.pull_request.number;
-        const message = "LGTM";
-        octokit.rest.issues.createComment({
-            ...github_1.context.repo,
-            issue_number: pull_number,
-            body: message,
-        });
-        octokit.rest.pulls.merge({
-            ...github_1.context.repo,
-            pull_number,
-        });
+        approve();
     }
     catch (error) {
         if (error instanceof Error) {
             core.setFailed(error.message);
         }
     }
+};
+const approve = () => {
+    const github_token = core.getInput("GITHUB_TOKEN");
+    const octokit = (0, github_1.getOctokit)(github_token);
+    const pull_number = github_1.context.payload.pull_request.number;
+    const message = "LGTM";
+    octokit.rest.issues.createComment({
+        ...github_1.context.repo,
+        issue_number: pull_number,
+        body: message,
+    });
+    // デバッグに差し支えるのでコメントアウト
+    // octokit.rest.pulls.merge({
+    //   ...context.repo,
+    //   pull_number,
+    // });
 };
 run();
 
--- src/index.ts
+++ src/index.ts
@@ -9,26 +9,30 @@ const run = async () => {
       return;
     }
 
-    // test用
-    const github_token = core.getInput("GITHUB_TOKEN");
-    const octokit = getOctokit(github_token);
-    const pull_number = context.payload.pull_request!.number;
-    const message = "LGTM";
-
-    octokit.rest.issues.createComment({
-      ...context.repo,
-      issue_number: pull_number,
-      body: message,
-    });
-    octokit.rest.pulls.merge({
-      ...context.repo,
-      pull_number,
-    });
+    approve();
   } catch (error) {
     if (error instanceof Error) {
       core.setFailed(error.message);
     }
   }
+};
+
+const approve = () => {
+  const github_token = core.getInput("GITHUB_TOKEN");
+  const octokit = getOctokit(github_token);
+  const pull_number = context.payload.pull_request!.number;
+  const message = "LGTM";
+
+  octokit.rest.issues.createComment({
+    ...context.repo,
+    issue_number: pull_number,
+    body: message,
+  });
+  // デバッグに差し支えるのでコメントアウト
+  // octokit.rest.pulls.merge({
+  //   ...context.repo,
+  //   pull_number,
+  // });
 };
 
 run();
